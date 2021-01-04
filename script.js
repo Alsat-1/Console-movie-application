a63353d71d875270fe9279c57869fb2c83f8f436
Stanislav <stas.bondarenko@ukr.net>
2021-01-05 01:43:23 +0200

    added conditionals and loops

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,12 +10,25 @@ const personalMovieDB = {
 	privat: false
 };
 
-const firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
-	secondQuestion = prompt('На сколько оцените его?', ''),
-	thirdQuestion = prompt('Один из последних просмотренных фильмов?', ''),
-	fourthQuestion = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+	let firstQuestion = prompt('Один из последних просмотренных фильмов?', ''),
+		secondQuestion = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[firstQuestion] = secondQuestion;
-personalMovieDB.movies[thirdQuestion] = fourthQuestion;
+	if (firstQuestion != null && secondQuestion != null && firstQuestion.length < 50 && firstQuestion != '' && secondQuestion != '') {
+		personalMovieDB.movies[firstQuestion] = secondQuestion;
+	} else {
+		i--;
+	}
+};
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+};
 
 console.log(personalMovieDB);
